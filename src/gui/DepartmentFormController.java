0619a5c8c13fbc714fe0f1cc05ca6fd305f01bfe
Felipe Lopes <felipearaujolopes@gmail.com>
2020-09-16 10:23:02 -0300

    Retornando aos estudos

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -91,7 +91,7 @@ public class DepartmentFormController implements Initializable {
 		
 		ValidationException exception = new ValidationException("Validation error");
 		
-		obj.setId(utils.tryParseToInt(txtId.getId()));
+		obj.setId(utils.tryParseToInt(txtId.getText()));
 		
 		if (txtName.getText()== null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can't be empty");
